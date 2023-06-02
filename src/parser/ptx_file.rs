f7a97c4cf0d95e1a09c25dce7056b5d7391c98a5
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-06-02 09:52:52 -0400

    testing example files

--- src/parser/ptx_file.rs
+++ src/parser/ptx_file.rs
@@ -1,6 +1,6 @@
-use nom::{branch::alt, Parser, character::complete::multispace0, sequence::preceded};
+use nom::{branch::alt, Parser, character::complete::multispace0, sequence::preceded, multi::many0_count};
 
-use super::{PtxFile, Function, Global, function::parse_function, global::parse_global, preamble::parse_preamble};
+use super::{PtxFile, Function, Global, function::parse_function, global::parse_global, preamble::parse_preamble, comment::comment_or_whitespace};
 
 pub(crate) enum FunctionOrGlobal<'a> {
     Function(Function<'a>),
@@ -12,7 +12,7 @@ impl<'a> Iterator for PtxFile<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         match preceded(
-            multispace0,
+            many0_count(comment_or_whitespace),
             alt((
                 parse_function.map(FunctionOrGlobal::Function),
                 parse_global.map(FunctionOrGlobal::Global),
@@ -22,7 +22,8 @@ impl<'a> Iterator for PtxFile<'a> {
                 self.body = body;
                 Some(value)
             },
-            Err(_) => None,
+            Err(e) => todo!("Error: {e:?}")
+            //Err(_) => None,
         }
     }
 }
@@ -31,21 +32,40 @@ impl<'a> TryFrom<&'a str> for PtxFile<'a> {
     type Error = nom::Err<nom::error::Error<&'a str>>;
 
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        let (body, preamble) = parse_preamble(value)?;
+        let (body, preamble) = preceded(
+            many0_count(comment_or_whitespace), 
+            parse_preamble
+        )(value)?;
         Ok(PtxFile { preamble, body })
     }
 }
 
+#[cfg(feature = "std")]
 #[cfg(test)]
 mod test_iterator {
     use super::{PtxFile, FunctionOrGlobal};
     use crate::parser::{Function, Global};
-    use crate::ptx_files::_EXAMPLE_FILE;
+    use crate::ptx_files::{_EXAMPLE_FILE, kernel, a, b, c, d};
 
-    #[cfg(feature = "std")]
     #[test]
     fn parse_example() {
         let ptx: PtxFile = _EXAMPLE_FILE.try_into().unwrap();
+        println!("Preamble: {preamble:?}", preamble = ptx.preamble);
+        for foo in ptx {
+            match foo {
+                FunctionOrGlobal::Function(function) => {
+                    println!("Function: {function:?}");
+                },
+                FunctionOrGlobal::Global(global) => {
+                    println!("Global: {global:?}");
+                },
+            }
+        }
+    }
+
+    #[test]
+    fn parse_kernel() {
+        let ptx: PtxFile = kernel::_PTX.try_into().unwrap();
         println!("Preamble: {preamble:?}", preamble = ptx.preamble);
         for foo in ptx {
             match foo {
